88a265aadb367f2261f0b8a3a7a61d2b133a4c84
madelinemc <madeline.coons@gmail.com>
2021-01-19 22:29:07 -0600

    commit tree e8d9b57a53b43014546cd1ee5c62b567992fd035

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+//returns a year the KC Chiefs won the superbowl & 'returns undefined if the record has no win objects',
+
+function superbowlWin(array) {
+  let win = array.find(function(arrayObj) {return arrayObj.result === 'W' })
+  return win ? win.year : win
+}
